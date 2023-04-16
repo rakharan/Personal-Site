e70c691f188308873c7ccad467ac3a8f42aa3ca2
Rakharan <randhikatamar@gmail.com>
2023-04-16 10:32:11 +0700

    little fix on navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,22 +6,22 @@ export default function Navbar() {
         <>
             <header>
                 <nav>
-                    <div className='h-[114px] px-2 w-full flex justify-between md:px-14'>
+                    <div className='h-[114px] px-5 w-full flex justify-between md:px-14'>
                         <div className='left-navbar  flex items-center'>
                             <Link href="/">
-                                <h1 className='text-5xl'>R<span className='font-bold'>.</span></h1>
+                                <h1 className='text-3xl md:text-5xl'>R<span className='font-bold'>.</span></h1>
                             </Link>
                         </div>
                         <div className='right-navbar  flex items-center'>
                             <ul className='flex gap-x-[30px]'>
                                 <li>
                                     <Link href="/photography">
-                                        <span className=' border-t-4 border-transparent hover:border-black transition-all duration-300 text-base p-2 leading-[50px] text-[12px]'>PHOTOGRAPHY</span>
+                                        <span className=' border-t-4 border-transparent hover:border-black transition-all duration-300 p-2 leading-[50px] text-sm md:text-lg'>PHOTOGRAPHY</span>
                                     </Link>
                                 </li>
                                 <li>
                                     <Link href="/resume">
-                                        <span className=' border-t-4 border-transparent hover:border-black transition-all duration-300 text-base p-2 leading-[50px] text-[12px]'>RESUME</span>
+                                        <span className=' border-t-4 border-transparent hover:border-black transition-all duration-300 text-sm p-2 leading-[50px] md:text-lg'>RESUME</span>
                                     </Link>
                                 </li>
                             </ul>
